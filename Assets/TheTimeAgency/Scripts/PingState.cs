b461b4e4087a1ce8cbc6830c23ef4b619b6357a4
Robert Dziuba <robertodziuba@gmail.com>
2016-12-08 22:21:37 +0100

    v3 wrapper class

--- Assets/TheTimeAgency/Scripts/PingState.cs
+++ Assets/TheTimeAgency/Scripts/PingState.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using UnityEngine;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using Assets.TheTimeAgency.Scripts;
 using Assets.TheTimeAgency.Scripts.KDTree;
@@ -23,18 +24,15 @@ public class PingState : ICrimeSceneState
 
     private readonly List<GameObject> _advicesList;
 
-    private readonly List<Vector3> _usedPoints;
-
     private float _maxX, _minX, _maxZ, _minZ;
 
-    private readonly SortedDictionary<float, List<Vector3>> _pointDic;
+    private readonly SortedDictionary<float, List<V3>> _pointDic;
 
     public PingState(CrimeScene crimeScenePattern)
     {
         crimeScene = crimeScenePattern;
         _advicesList = new List<GameObject>();
-        _pointDic = new SortedDictionary<float, List<Vector3>>();
-        _usedPoints =  new List<Vector3>();
+        _pointDic = new SortedDictionary<float, List<V3>>();
     }
 
     public void StartState()
@@ -65,14 +63,12 @@ public class PingState : ICrimeSceneState
         return (viewportPoint.z > 0 && (new Rect(0, 0, 1, 1)).Contains(viewportPoint) && viewportPoint.z < Camera.main.farClipPlane);
     }
 
-    private Vector3[] PointCloudPointsICameraView()
+    private void PointCloudPointsInCameraView()
     {
         System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
 
         // Begin timing.
         stopwatch.Start();
-
-        List<Vector3> points = new List<Vector3>();
 
         foreach (Vector3 point in crimeScene.m_pointCloud.m_points)
         {
@@ -82,19 +78,19 @@ public class PingState : ICrimeSceneState
                 {
                     if (crimeScene.triangleList[0].PointInTriangle(point) || crimeScene.triangleList[1].PointInTriangle(point))
                     {
-                        points.Add(point);
                         // Group similar points into buckets based on sensitivity. 
                         float roundedY = Mathf.Round(point.y / SENSITIVITY) * SENSITIVITY;
 
                         if (!_pointDic.ContainsKey(roundedY))
                         {
-                            _pointDic.Add(roundedY, new List<Vector3>());
+                            _pointDic.Add(roundedY, new List<V3>());
                         }
 
-                        if (!_pointDic[roundedY].Contains(point))
+                        var v = new V3(point);                   
+                        if (!_pointDic[roundedY].Contains(v))
                         {
-                            _pointDic[roundedY].Add(point);
-                        };
+                            _pointDic[roundedY].Add(v);
+                        }
 
                     }
                 }
@@ -104,15 +100,14 @@ public class PingState : ICrimeSceneState
         stopwatch.Stop();
 
         // Write result.
-        Debug.Log(string.Format("Time elapsed for PointCloudPointsICameraView: {0}", stopwatch.Elapsed));
-    
-        return points.ToArray(); ;
+        Debug.Log(string.Format("Time elapsed for PointCloudPointsICameraView: {0}", stopwatch.Elapsed));;
     }
 
     
 
     public void UpdateState()
     {
+
         if (m_ping)
         {
             if (crimeScene.m_AdvicePlaceHolderList.Count <= 0) return;
@@ -124,7 +119,7 @@ public class PingState : ICrimeSceneState
 
             m_ping = false;
 
-            Vector3[] pointCloudPointList = PointCloudPointsICameraView();
+            PointCloudPointsInCameraView();
 
             Debug.Log(string.Format("dic: {0}", _pointDic.Count));
 
@@ -133,48 +128,51 @@ public class PingState : ICrimeSceneState
 
                 var pointListCopy = _pointDic[key].ToList();
 
-                Debug.Log(string.Format("Wir sind in der Y-Koordinate: {0} mit {1} points", key, pointListCopy.Count));
+                Debug.Log(string.Format("Time elapsed for pointListCopy: {0}", stopwatch.Elapsed));
 
-                while (pointListCopy.Count > 10)
-                {
+                //Debug.Log(string.Format("Wir sind in der Y-Koordinate: {0} mit {1} points", key, pointListCopy.Count));
 
-                    List<Vector3> tempUsedPoints = new List<Vector3>();
+                /*while (pointListCopy.Count > 10)
+                {*/
+                KDTree<V3> pTree = CreateVector2KDTree(pointListCopy);
 
-                    KDTree<Vector3> pTree = CreateVector2KDTree(pointListCopy, tempUsedPoints);
+                Debug.Log(string.Format("Time elapsed for KDTree: {0}", stopwatch.Elapsed));
 
-                    int pos = Random.Range(0, pointListCopy.Count - 1);
-                    var pIter = pTree.NearestNeighbors(new double[] {pointListCopy[pos].x, pointListCopy[pos].z},
-                        pointListCopy.Count, 0.15f);
+                int pos = Random.Range(0, pointListCopy.Count - 1);
+                var pIter = pTree.NearestNeighbors(new double[] {pointListCopy[pos].x, pointListCopy[pos].z}, pointListCopy.Count, 0.15f);
 
-                    int counter = 0;
-                    int numOnLine = 0;
+                Debug.Log(string.Format("Time elapsed for NearestNeighbors: {0}", stopwatch.Elapsed));
 
-                    Vector3 average = CalcAverageOfArea(pIter, ref counter, ref numOnLine, pointListCopy);
+                int counter = 0;
+                int numOnLine = 0;
+                bool _checked = new Boolean();
+                _checked = false;
 
-                    Debug.Log("Points im Radius: " + counter);
-                    Debug.Log(string.Format("Davon in einer Linie {0} von {1} / {2}%", numOnLine, counter,
-                        numOnLine*1.0f/counter*100.0f));
+                Vector3 average = CalcAverageOfArea(pIter, ref counter, ref numOnLine, pointListCopy, ref _checked);
 
-                    if (counter < 10 || numOnLine*1.0f/counter*100.0f >= 80) continue;
+                Debug.Log(string.Format("Time elapsed for CalcAverageOfArea: {0}", stopwatch.Elapsed));
 
-                    _usedPoints.AddRange(tempUsedPoints);
+                //Debug.Log("Points im Radius: " + counter);
+                //Debug.Log(string.Format("Davon in einer Linie {0} von {1} / {2}%", numOnLine, counter, numOnLine*1.0f/counter*100.0f));
 
-                    Debug.Log(string.Format("used: {0}", _usedPoints.Count));
+                if (counter < 10 || numOnLine*1.0f/counter*100.0f >= 80) continue;
 
-                    bool outOfReach = !InReachToOutherAdvices(average);
+                bool outOfReach = !InReachToOutherAdvices(average);
 
-                    Debug.Log(string.Format("Stelle gefunden bei: {0} und hat genügend Abstand: {1}", average,
-                        outOfReach));
+                Debug.Log(string.Format("Time elapsed for InReachToOutherAdvices: {0}", stopwatch.Elapsed));
 
-                    if (!outOfReach) continue;
+                //Debug.Log(string.Format("Stelle gefunden bei: {0} und hat genügend Abstand: {1}", average, outOfReach));
 
-                    Color color = Random.ColorHSV();
+                if (!outOfReach) continue;
 
-                    GameObject advice = AddCube("advice_" + average.x + "/" + average.y + "/" + average.z, average,
-                        color);
+                _checked = true;
 
-                    _advicesList.Add(advice);
-                }
+                Color color = Random.ColorHSV();
+
+                GameObject advice = AddCube("advice_" + average.x + "/" + average.y + "/" + average.z, average, color);
+
+                _advicesList.Add(advice);
+               /* }*/
             }
 
             stopwatch.Stop();
@@ -191,7 +189,7 @@ public class PingState : ICrimeSceneState
 
     }
 
-    private Vector3 CalcAverageOfArea(NearestNeighbour<Vector3> pIter, ref int counter, ref int numOnLine, List<Vector3> pointList )
+    private Vector3 CalcAverageOfArea(NearestNeighbour<V3> pIter, ref int counter, ref int numOnLine, List<V3> pointList, ref bool _checked )
     {
         Vector3 sum = Vector3.zero;
         Vector3 a = Vector3.zero;
@@ -199,7 +197,10 @@ public class PingState : ICrimeSceneState
 
         while (pIter.MoveNext())
         {
-            var point = pIter.Current;
+
+            var v3 = pIter.Current;
+            v3.Checked = _checked;
+            var point = v3.vec3;
 
             sum += point;
 
@@ -216,25 +217,27 @@ public class PingState : ICrimeSceneState
                 numOnLine++;
             }
 
-            pointList.Remove(point);
+            pointList.Remove(new V3(point));
         }
 
         return sum/counter;
     }
 
-    private KDTree<Vector3> CreateVector2KDTree(List<Vector3> pointList, List<Vector3> tempUsedPoints)
+    private KDTree<V3> CreateVector2KDTree(List<V3> pointList)
     {
 
-        KDTree<Vector3> KDTree = new KDTree<Vector3>(2);
+        KDTree<V3> KDTree = new KDTree<V3>(2);
 
         foreach (var point in pointList)
         {
-            if (!_usedPoints.Contains(point))
-            {
+
+            //Debug.Log(string.Format("This point is checked", point.Checked));
+
+            /*if (!point.Checked)
+            {*/
                 //pTree.AddPoint(new double[] { x, y }, new EllipseWrapper(x, y));
-                KDTree.AddPoint(new double[] { point.x, point.z }, point);
-                tempUsedPoints.Add(point);
-            }
+                KDTree.AddPoint(new double[] {point.x, point.z}, point);
+            //}
         }
 
         return KDTree;
@@ -275,7 +278,10 @@ public class PingState : ICrimeSceneState
             if (GUI.Button(new Rect(Screen.width - 220, 20, 200, 80), "<size=30>Ping</size>")) m_ping = true;
         }
 
-        m_show = GUI.Toggle(new Rect(Screen.width - 220, Screen.height - 100, 200, 80), m_show, "<size=30>Show</size>");
+        if (!m_show)
+        {
+            if (GUI.Button(new Rect(Screen.width - 220, Screen.height - 100, 200, 80), "<size=30>Show</size>")) m_show = true;
+        }
 
         GUI.Label(new Rect(0, Screen.height - 50, Screen.width, 50), string.Format("<size=30> {0} / {1} Aadvices added!</size>", _advicesList.Count, crimeScene.m_numberAdvices));
     }
---
+++ Assets/TheTimeAgency/Scripts/V3.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class V3
+{
+    public float x
+    {
+        get { return _vec3.x; }
+
+        set
+        {
+            _vec3.x = value;
+        }
+    }
+
+    public float y
+    {
+        get { return _vec3.y; }
+
+        set
+        {
+            _vec3.y = value;
+        }
+
+    }
+
+    public float z
+    {
+        get { return _vec3.z; }
+
+        set
+        {
+            _vec3.z = value;
+        }
+    }
+
+    public bool Checked { get; set; }
+
+    private Vector3 _vec3;
+
+    public Vector3 vec3
+    {
+        get { return _vec3; }
+    }
+
+    public V3(Vector3 v3)
+    {
+        _vec3 = v3;
+    }
+}
